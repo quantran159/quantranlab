6aefd3ab877b937107d6c3501a83f3a029e94286
quantran159 <tranmanhquan159@gmail.com>
2026-08-14 10:58:24 +0700

    Restore corrupted postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,3 @@
-Exit code: 0
-Wall time: 1.2 seconds
-Output:
 const postcssConfig = {
   plugins: {
     "@tailwindcss/postcss": {},
@@ -8,4 +5,3 @@ const postcssConfig = {
 };
 
 export default postcssConfig;
-
